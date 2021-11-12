3c6b56140290e2889f5ecdbb4f4f4716941129d2
MartaMalovana <marta.malovana@gmail.com>
2021-11-13 00:50:04 +0200

    Намальовано результат запиту 12 карток

--- src/apiService.js
+++ src/apiService.js
@@ -1,7 +1,18 @@
+import cardRender from './image_card.hbs';
+
+
 const myKey = '24296809-9b93a2a7fdd6c9a326bbfa052';
 let pageNumber = 1;
+const cardList = document.querySelector('.gallery');
+
+
 
 export default function getImages (value) {
+    const makeCard = function (arr) {
+        cardList.insertAdjacentHTML('beforeend', cardRender(arr));
+    };
+
     const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${value}&page=${pageNumber}&per_page=12&key=${myKey}`;
-    fetch(url).then(result => result.json(''));
+    return fetch(url).then(result => result.json('')).then(array => makeCard(array.hits));
 } 
+       
--- src/index.js
+++ src/index.js
@@ -2,29 +2,11 @@ import './sass/main.scss';
 import 'material-design-icons';
 import { debounce } from 'debounce';
 import getImages from './apiService';
-import cardRender from './image_card.hbs';
 
 const inputValueRef = document.querySelector('[name="query"]');
-const cardList = document.querySelector('.gallery');
 
-const createCard = function (card) {
-  // cardList.insertAdjacentHTML('beforeend', cardRender(card.hits[0]));
-  console.log(card);
-} 
-
-async function searchImage () {
-   const imagesArray = await function () {
-    const a= getImages(inputValueRef.value);
-     return console.log(a);
-   };
-
-   const makeCard = await function (images) {
-     return createCard(images);
-   };
-
-   imagesArray();
-   makeCard(imagesArray);
-
+const searchImage = function () {
+  getImages(inputValueRef.value);
 };
 
 inputValueRef.addEventListener('input', debounce(searchImage, 1000));
